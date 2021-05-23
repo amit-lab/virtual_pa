fb58b5c162db7ac0799e4370e165722bcfd9ca32
Amit Dongarwar <55277797+amit-lab@users.noreply.github.com>
2021-05-23 11:23:18 +0530

    Create user_data.py

---
+++ user_data.py
@@ -0,0 +1,8 @@
+# open weather api  grab it from "https://openweathermap.org/api"
+open_weather_api_key = YOUR_API_KEY
+
+# Twilio api  grab these things from "https://www.twilio.com/docs/usage/api"
+twilio_account_sid = ""
+twilio_auth_token = ""
+twilio_from = ""
+twilio_to = ""
